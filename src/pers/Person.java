6478ee7ae18cc5ca0c0380dd2a890f92ca755f4f
Moheto22 <mohatae61@gmail.com>
2024-02-23 09:48:02 +0100

    commit

--- src/pers/Person.java
+++ src/pers/Person.java
@@ -1,4 +1,19 @@
 package pers;
 
+import teams.Team;
+
 public abstract class Person {
+    protected final String name;
+    protected final int age;
+    protected final String nation;
+    protected final String surname;
+    protected Team club;
+    protected double motivation;
+    protected int salary;
+    protected Person(String name, int age, String nation, String surname) {
+        this.name = name;
+        this.age = age;
+        this.nation = nation;
+        this.surname = surname;
+    }
 }
