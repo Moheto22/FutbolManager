6e56f48b81cbf215d58219da4055bbf8c148b1db
Moheto22 <mohatae61@gmail.com>
2024-02-23 10:07:42 +0100

    commit

--- src/pers/Person.java
+++ src/pers/Person.java
@@ -4,16 +4,48 @@ import teams.Team;
 
 public abstract class Person {
     protected final String name;
-    protected final int age;
+    protected final int yearOfBirth;
     protected final String nation;
     protected final String surname;
     protected Team club;
     protected double motivation;
     protected int salary;
-    protected Person(String name, int age, String nation, String surname) {
+    protected Person(String name, int yearOfBirth, String nation, String surname) {
         this.name = name;
-        this.age = age;
+        this.yearOfBirth = yearOfBirth;
         this.nation = nation;
         this.surname = surname;
+
+    }
+    protected void training() {
+        this.motivation += 0.2;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public String getNation() {
+        return nation;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public Team getClub() {
+        return club;
+    }
+
+    public double getMotivation() {
+        return motivation;
+    }
+
+    public int getSalary() {
+        return salary;
     }
 }
--- src/pers/Player.java
+++ src/pers/Player.java
@@ -1,4 +1,18 @@
 package pers;
 
-public class Player {
+public class Player extends Person{
+    private int gols;
+    private double quality;
+    private static int numberOfPlayers;
+    private String position;
+    private String stylePlaying;
+    protected Player(String name, int age, String nation, String surname) {
+        super(name, age, nation, surname);
+    }
+    @Override
+    protected void training() {
+        super.training();
+        numberOfPlayers++;
+    }
+
 }
